303717bf4deb85ad55a56a73b78293d10c7bbe9b
Peter Dunne <peter.dunne@applied-magnetism.com>
2021-06-03 18:50:16 +0200

    STL Examples
    STL Examples now Colab compatible

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import pathlib
 here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+long_description = (here / "pypi_README.md").read_text(encoding="utf-8")
 
 
 setup(
